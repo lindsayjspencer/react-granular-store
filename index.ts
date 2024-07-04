09751ebbb87cd52c5a10c5c5af352b0bdfecb3c9
Lindsay Spencer <lindsay.spencer@codebots.com>
2024-07-04 17:16:06 +1000

    Allow string and numbers in hook overloads for RecordStore

--- index.ts
+++ index.ts
@@ -103,8 +103,8 @@ export function useStoreValue<State extends StateTree, Key extends keyof State>(
 	store: Store<State> | null,
 	key: Key,
 ): State[Key] | null;
-export function useStoreValue<T>(store: RecordStore<T>, key: string): T;
-export function useStoreValue<T>(store: RecordStore<T> | null, key: string): T | null;
+export function useStoreValue<T>(store: RecordStore<T>, key: string | number): T;
+export function useStoreValue<T>(store: RecordStore<T> | null, key: string | number): T | null;
 export function useStoreValue<State extends StateTree, Key extends keyof State>(store: Store<State> | null, key: Key) {
 	const [state, setState] = useState(store?.getState(key) ?? null);
 
@@ -125,8 +125,8 @@ export function useStoreUpdate<State extends StateTree, Key extends keyof State>
 	store: Store<State> | null,
 	key: Key,
 ): (newValue: SetStateArgument<State[Key]>) => void;
-export function useStoreUpdate<T>(store: RecordStore<T>, key: string): (newValue: SetStateArgument<T>) => void;
-export function useStoreUpdate<T>(store: RecordStore<T> | null, key: string): (newValue: SetStateArgument<T>) => void;
+export function useStoreUpdate<T>(store: RecordStore<T>, key: string | number): (newValue: SetStateArgument<T>) => void;
+export function useStoreUpdate<T>(store: RecordStore<T> | null, key: string | number): (newValue: SetStateArgument<T>) => void;
 export function useStoreUpdate<State extends StateTree, Key extends keyof State>(store: Store<State> | null, key: Key) {
 	return useCallback(
 		(newValue: SetStateArgument<State[Key]>) => {
@@ -145,8 +145,8 @@ export function useStoreState<State extends StateTree, Key extends keyof State>(
 	store: Store<State> | null,
 	key: Key,
 ): [State[Key] | null, (newValue: SetStateArgument<State[Key]>) => void];
-export function useStoreState<T>(store: RecordStore<T>, key: string): [T, (newValue: SetStateArgument<T>) => void];
-export function useStoreState<T>(store: RecordStore<T> | null, key: string): [T | null, (newValue: SetStateArgument<T>) => void];
+export function useStoreState<T>(store: RecordStore<T>, key: string | number): [T, (newValue: SetStateArgument<T>) => void];
+export function useStoreState<T>(store: RecordStore<T> | null, key: string | number): [T | null, (newValue: SetStateArgument<T>) => void];
 export function useStoreState<State extends StateTree, Key extends keyof State>(store: Store<State> | null, key: Key) {
 	const state = useStoreValue(store, key);
 	const updateState = useStoreUpdate(store, key);
